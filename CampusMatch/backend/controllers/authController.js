06cd5d6fe6d8bf2ea85ba6c8846fbe6de1248a32
Isabella Rodrigues Quesada <allebasirq@gmail.com>
2026-07-11 17:18:27 -0400

    Implement user login with JWT

--- CampusMatch/backend/controllers/authController.js
+++ CampusMatch/backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 // Register user
 const register = async (req, res) => {
@@ -39,6 +40,61 @@ const register = async (req, res) => {
     }
 };
 
+// Login user
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        // Find user
+        const user = await User.findOne({ email });
+
+        if (!user) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        // Check password
+        const isMatch = await bcrypt.compare(
+            password,
+            user.passwordHash
+        );
+
+        if (!isMatch) {
+            return res.status(400).json({
+                message: "Invalid email or password"
+            });
+        }
+
+        // Create JWT token
+        const token = jwt.sign(
+            {
+                userId: user._id,
+                email: user.email
+            },
+            process.env.JWT_SECRET,
+            {
+                expiresIn: "1d"
+            }
+        );
+
+        res.status(200).json({
+            message: "Login successful",
+            token,
+            user: {
+                id: user._id,
+                email: user.email
+            }
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+};
+
 module.exports = {
-    register
+    register,
+    login
 };
